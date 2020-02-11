e538060e82ef4b82b07f874ce8d12e9768b63607
易 <yi@leegame.net>
2020-02-11 20:24:54 +0800

    Update - revise

--- SwiftAdvance/SwiftAdvance/ViewController.swift
+++ SwiftAdvance/SwiftAdvance/ViewController.swift
@@ -12,23 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        mergeDict()
+        iteratorUse()
     }
 
     
     func usedReduce() {
         let temp = [1, 3, 5, 7]
-        let result = temp.reduce(0) { (total, item) in
-            total + item
-        }
+//        let result = temp.reduce(0) { (total, item) in
+//            total + item
+//        }
+
+        let result = temp.reduce(0, +)
         print(result)
-//        let result2 = temp.reduce(0, +)
     }
     
-    func usedReduse2() {
-        let temp = [1, 3, 5, 7]
-//        temp.reduce(<#T##initialResult: Result##Result#>, <#T##nextPartialResult: (Result, Int) throws -> Result##(Result, Int) throws -> Result#>)
-//        temp.reduce(into: <#T##Result#>, <#T##updateAccumulatingResult: (inout Result, Int) throws -> ()##(inout Result, Int) throws -> ()#>)
+    func usedReduce2() {
+        let temp = "asdgqasdfasdfa"
+        let xx = temp.reduce(into: [:]) { (result, charactor) in
+            result[charactor, default: 0] += 1
+        }
+        print(xx)
     }
     
     func mapAndFlatMap() {
@@ -43,9 +46,9 @@ class ViewController: UIViewController {
     }
     
     func compactMap() {
-        let numbers = ["1", "2", "three", "///5///", "5"]
+        let numbers = ["1", "2", "gour", "///5///", "5"]
 
-        // 此时用flatMap会报警告，应为返回值是可选的， compactMap为之前flatMap的去空方法的重构方法名
+        // 此时用flatMap会报警告，因为返回值是可选的， compactMap为之前flatMap的去空方法的重构方法名
         let mapResult = numbers.compactMap { (number) -> Int? in
             return Int(number)
         }
@@ -68,18 +71,77 @@ class ViewController: UIViewController {
         let _ = Array(temp)
     }
     
+    //MARK: Dictionary
     func mergeDict() {
         var dict1 = ["name": "Tom", "age": "3"]
         let dict2 = ["name": "Jerry", "age": "3", "sex": "man"]
         dict1.merge(dict2) { (t1, t2) -> String in
             return t1            
         }
-        
+        print(dict1)
         
         let temp = "helloworld".map { ($0, 1)}
         print(temp)
         print(Dictionary(temp, uniquingKeysWith: +))
     }
+    
+    //MARK: Set
+    func subtracting() {
+        let t1: Set = ["1", "2", "3", "4"]
+        let t2: Set = ["2", "5"]
+        let t3 = t2.subtracting(t1) // ["5"]
+        //let t4 = t1.subtracting(t2) // ["1", "3", "4"]
+        print(t3)
+    }
+    
+    func intersection() {
+        let t1: Set = ["1", "2", "3", "4"]
+        let t2: Set = ["2", "5"]
+        let t3 = t1.intersection(t2) //["2"]
+        print(t3)
+    }
+    
+    func formUnion() {
+        var t1: Set = ["1", "2", "3", "4"]
+        let t2: Set = ["2", "5"]
+        t1.formUnion(t2) //["3", "4", "5", "1", "2"]
+        print(t1)
+    }
+    
+    //MARK: Iterator
+    func iteratorUse() {
+        for item in PrefixSequence(string: "hello word") {
+            print(item)
+        }
+    }
 }
 
 
+struct PrefixtIterator: IteratorProtocol {
+    let string: String
+    var offSet: String.Index
+    
+    init(string: String) {
+        self.string = string
+        offSet = string.startIndex
+    }
+    
+    mutating func next() -> Substring? {
+        guard offSet < string.endIndex else {
+            return nil
+        }
+        offSet = string.index(after: offSet)
+        return string[..<offSet]
+    }
+    
+}
+
+
+struct PrefixSequence: Sequence {
+    let string: String
+    
+    __consuming func makeIterator() -> PrefixtIterator {
+        return PrefixtIterator(string: string)
+    }
+    
+}
